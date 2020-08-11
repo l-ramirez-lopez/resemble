b99624dabe48982652a558dff81e69e52a77411d
Alexandre Wadoux <33750884+AlexandreWadoux@users.noreply.github.com>
2020-08-12 08:26:04 +1000

    add data and rd files
    large and local vis-NIR data are added with basic documentation.

---
+++ R/large_visNIR.R
@@ -0,0 +1,18 @@
+#' @docType data
+#' @name large_visNIR
+#' @aliases large_visNIR
+#' @title large_visNIR
+#' @format A `data.frame` of 742 observations and 3 variables (where the spectral data is embedded in one variable `large_visNIR$spc`).
+#' @usage
+#' data(large_visNIR)
+#' @description
+#' Soil spectral library containing soil clay values in percent and the vis-NIR soil spectra.
+#' The database contains absorbance spectra of dried and sieved soil samples measured between 350 nm
+#' and 2500 nm at 2 nm interval. One soil property is associated with the spectral library: clay content in percent. Each spectrum has a unique id value.
+#' @details
+#' No further details.
+#' @source Raphael Viscarra-Rossel
+#' @references
+#' Rossel, R. V., & Behrens, T. (2010). Using data mining to model and interpret soil diffuse reflectance spectra. Geoderma, 158(1-2), 46-54.
+#' @keywords datasets
+NULL
