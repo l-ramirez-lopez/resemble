dabd26a6222c86d8ede7ec19c414d05e63b7dc6a
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2020-10-19 15:41:10 +0200

    arma input matrices

--- src/moving_cor_diss.cpp
+++ src/moving_cor_diss.cpp
@@ -1,6 +1,8 @@
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
+using namespace RcppArmadillo;
+
 
 //' @title Moving/rolling correlation distance of two matrices
 //' @description Computes a moving window correlation distance between two data matrices
@@ -14,20 +16,24 @@ using namespace Rcpp;
 //' @useDynLib resemble
 //' @author Leonardo Ramirez-Lopez and Antoine Stevens
 // [[Rcpp::export]]
-NumericMatrix moving_cor_diss(NumericMatrix X, NumericMatrix Y,  int w){  
-  arma::mat XX(X.begin(), X.nrow(), X.ncol(), false);
-  arma::mat YY(Y.begin(), Y.nrow(), Y.ncol(), false);    
-  arma::mat rmwF = arma::zeros(X.nrow(),Y.nrow());
-  if(!w%2) throw exception("'w' must be odd") ;
-  int gap = (w-1)/2;
-  if((Y.ncol() - w ) < 1){
-    arma::mat rmwF = arma::cor(XX.t(), YY.t());
-  } else {
-    for(int i = gap; i < Y.ncol() - gap; i++){
-      rmwF += arma::cor(XX.cols(i-gap,i+gap).t(), YY.cols(i-gap,i+gap).t()); // sum of the correlations
-    }
-    rmwF = rmwF/(Y.ncol()- (2*gap)); // get the average
+NumericMatrix moving_cor_diss(arma::mat X, arma::mat Y, int w){  
+  arma::mat rmwF = arma::zeros(X.n_rows, Y.n_rows);
+  if(!w%2) {
+    throw std::invalid_argument("w must be odd");
   }
-  arma::mat scmw = (1 - rmwF)/2;  // get the distance
+  int gap = (w - 1) / 2;
+  if((Y.n_cols - w ) < 1){
+    arma::mat rmwF = arma::cor(trans(X), trans(Y));
+  } else {
+    int ny = Y.n_cols;
+    for(int i = gap; i < ny - gap; i++){
+      // sum of the correlations
+      rmwF += arma::cor(X.cols(i-gap, i + gap).t(), Y.cols(i - gap, i + gap).t()); 
+    }
+    // get the average
+    rmwF = rmwF / (Y.n_cols - (2*gap)); 
+  }
+  // get the distance
+  arma::mat scmw = (1 - rmwF)/2;  
   return wrap(scmw.t());
 }
