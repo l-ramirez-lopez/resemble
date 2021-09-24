32873c742d5bb67d964c5f784f655d3d511ff286
Leonardo Ramirez-Lopez <ramirez.lopez.leo@gmail.com>
2021-09-25 00:27:11 +0200

    Update print.local_fit.R

--- R/print.local_fit.R
+++ R/print.local_fit.R
@@ -13,12 +13,22 @@ print.local_fit <- function(x, ...) {
   }
 
   if (x$method == "pls") {
-    cat("Partial least squares (pls)")
+    if (x$modified) {
+      mss <- "Modified partial least squares (mpls)"
+    } else {
+      mss <- "Partial least squares (pls)"
+    }
+    cat(mss)
     cat("\nNumber of factors:", x$pls_c)
   }
 
   if (x$method == "wapls") {
-    cat("Weighted average partial least squares (wapls)")
+    if (x$modified) {
+      mss <- "Weighted average modified partial least squares (wampls)"
+    } else {
+      mss <- "Weighted average partial least squares (wapls)"
+    }
+    cat(mss)
     cat("\nMin. and max. number of factors: from", x$pls_c[["min_pls_c"]], "to", x$pls_c[["max_pls_c"]])
   }
 
