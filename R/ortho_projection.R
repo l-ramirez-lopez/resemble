ca7316d267e0c923be47dd86e9820ed59e4a2e6e
l-ramirez-lopez <leonardo.ramirez-lopez23@imperial.ac.uk>
2026-04-14 19:32:40 +0200

    doc: include plot

--- R/ortho_projection.R
+++ R/ortho_projection.R
@@ -4,7 +4,24 @@
 #' \loadmathjax
 #' Performs orthogonal projections of high-dimensional data matrices using
 #' principal component analysis (PCA) or partial least squares (PLS).
+#' 
+#' @usage 
+#' ortho_projection(
+#'   Xr, Xu = NULL, Yr = NULL,
+#'   ncomp = ncomp_by_var(0.01),
+#'   method = c("pca", "pca_nipals", "pls", "mpls", "simpls"),
+#'   center = TRUE,
+#'   scale = FALSE,
+#'   tol = 1e-6,
+#'   max_iter = 1000L,
+#'   pc_selection = deprecated(),
+#'   ...
+#' )
+#' 
+#' \method{predict}{ortho_projection}(object, newdata, ...)
 #'
+#' \method{plot}{ortho_projection}(x, col = "#3B82F6", ...)
+#' 
 #' @param Xr A numeric matrix of reference observations (rows) and variables
 #'   (columns).
 #' @param Xu An optional matrix of additional observations to project.
@@ -37,6 +54,9 @@
 #' @param max_iter Maximum number of iterations for NIPALS. Default is
 #'   \code{1000}. Ignored when \code{method = "simpls"}.
 #' @param pc_selection `r lifecycle::badge("deprecated")` Use \code{ncomp} instead.
+#' @param x An object of class \code{ortho_projection} (as returned by
+#'   \code{\link{ortho_projection}}).
+#' @param col Color for the plot elements. Default is \code{"#3B82F6"}.
 #' @param ... Additional arguments (currently unused).
 #'
 #' @details
@@ -797,4 +817,93 @@ predict.ortho_projection <- function(object, newdata, ...) {
   }
   
   result
+}
+
+
+#' @aliases ortho_projection
+#' @importFrom graphics barplot grid par plot segments
+#' @export
+plot.ortho_projection <- function(x, col = "#3B82F6", ...) {
+  
+  if (inherits(x$ncomp_method, "ncomp_by_opc")) {
+    .plot_opc_evaluation(x, col = col, ...)
+  } else {
+    .plot_variance(x, col = col, ...)
+  }
+  
+  invisible(NULL)
+}
+
+# --- internal: OPC evaluation plot --------------------------------------------
+.plot_opc_evaluation <- function(x, col, ...) {
+  opc_eval <- x$opc_evaluation
+  tpl <- opc_eval[, c(1, ncol(opc_eval))]
+  metric <- colnames(tpl)[2]
+  
+  ylab <- switch(
+    metric,
+    mean_standardized_rmsd_Yr = "Mean standardized RMSD",
+    rmsd_Yr = "RMSD",
+    rmsd = "RMSD",
+    kappa = "Kappa",
+    metric
+  )
+  
+  plot(
+    tpl,
+    type = "p",
+    pch = 16,
+    col = col,
+    ylab = ylab,
+    xlab = "Number of components",
+    las = 1,
+    ...
+  )
+  grid(col = "#33415580", lty = 1)
+  segments(tpl[, 1], 0, tpl[, 1], tpl[, 2], col = col)
+  
+  # Mark optimal
+  opt_idx <- which.min(tpl[, 2])
+  if (metric == "kappa") {
+    opt_idx <- which.max(tpl[, 2])
+  }
+  points(tpl[opt_idx, 1], tpl[opt_idx, 2], pch = 16, col = "#F59E0B", cex = 1.5)
+}
+
+# --- internal: variance plot --------------------------------------------------
+.plot_variance <- function(x, col, ...) {
+  x_var <- x$variance$x_var
+  
+  opar <- par(mfrow = c(1, 2), mar = c(4, 4, 2, 1))
+  on.exit(par(opar))
+  
+  # Individual variance
+  ind_var <- x_var[grep("^explained_var", rownames(x_var)), ]
+  barplot(
+    ind_var,
+    horiz = FALSE,
+    names.arg = colnames(x_var),
+    ylim = c(0, 1),
+    ylab = "Explained variance",
+    xlab = "Component",
+    col = col,
+    border = NA,
+    las = 1,
+    ...
+  )
+  
+  # Cumulative variance
+  cum_var <- x_var[grep("cumulative", rownames(x_var)), ]
+  barplot(
+    cum_var,
+    horiz = FALSE,
+    names.arg = colnames(x_var),
+    ylim = c(0, 1),
+    ylab = "Cumulative explained variance",
+    xlab = "Component",
+    col = col,
+    border = NA,
+    las = 1,
+    ...
+  )
 }
